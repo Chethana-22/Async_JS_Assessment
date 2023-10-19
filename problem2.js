7b563f0910f09f31144ba5c2f8d685192c738099
Chethana-22 <chethanasv22@gmail.com>
2023-10-19 11:40:25 +0530

    initial commit

--- problem2.js
+++ problem2.js
@@ -1,12 +1,12 @@
 const fs = require('fs').promises;
 
-                                    async function readAndConvertToUppercase() {
-  try                     {
+async function readAndConvertToUppercase() {
+  try {
     const data = await fs.readFile('lipsum.txt', 'utf-8');
     const uppercaseContent = data.toUpperCase();
-                                                          const filename = 'uppercase.txt'
+    const filename = 'uppercase.txt';
     await fs.writeFile(filename, uppercaseContent);
-              fs.appendFile('filenames.txt',filename + '\n');
+    fs.appendFile('filenames.txt', filename + '\n');
     return 'uppercase.txt';
   } catch (error) {
     throw new Error(`Error in readAndConvertToUppercase: ${error.message}`);
@@ -18,15 +18,15 @@ async function readUppercaseAndSplit() {
   try {
     const data = await fs.readFile('uppercase.txt', 'utf-8');
     const sentences = data.split(/[.!?]/);
-    
+
     let filename;
     for (let i = 0; i < sentences.length; i++) {
       const sentence = sentences[i].trim();
       if (sentence.length > 0) {
-         filename = `sentence_${i + 1}.txt`;
+        filename = `sentence_${i + 1}.txt`;
         await fs.writeFile(filename, sentence);
         sentenceFiles.push(sentence);
-        await fs.appendFile('filenames.txt',filename + '\n');
+        await fs.appendFile('filenames.txt', filename + '\n');
       }
     }
     return sentenceFiles;
@@ -40,7 +40,7 @@ async function readSentencesSortAndWrite() {
     const sortedSentences = sentenceFiles.sort().join('\n');
     const filename = 'sorted.txt';
     await fs.writeFile(filename, sortedSentences);
-    fs.appendFile('filenames.txt',filename + '\n');
+    fs.appendFile('filenames.txt', filename + '\n');
     return 'sorted.txt';
   } catch (error) {
     throw new Error(`Error in readSentencesSortAndWrite: ${error.message}`);
@@ -50,12 +50,12 @@ async function readSentencesSortAndWrite() {
 async function readFilenamesAndDelete() {
   try {
     const data = await fs.readFile('filenames.txt', 'utf-8');
-    const fileList = data.split('\n').map(line => line.trim());
+    const fileList = data.split('\n').map((line) => line.trim());
 
     for (const filename of fileList) {
       try {
         if (filename) {
-          await fs.unlink(filename); 
+          await fs.unlink(filename);
           console.log(`Deleted: ${filename}`);
         }
       } catch (err) {
@@ -71,11 +71,11 @@ async function readFilenamesAndDelete() {
 
 async function main() {
   try {
-   await readAndConvertToUppercase();
-    await readUppercaseAndSplit(); 
+    await readAndConvertToUppercase();
+    await readUppercaseAndSplit();
     await readSentencesSortAndWrite();
     await readFilenamesAndDelete();
-   
+
     console.log('All tasks completed successfully.');
   } catch (error) {
     console.error(`An error occurred: ${error.message}`);
@@ -83,4 +83,3 @@ async function main() {
 }
 
 main();
-
